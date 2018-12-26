37384b5bff19cbc0229247eba28d7d7c0bd5077a
Jim Yang <jyang_123@hotmail.com>
2018-12-26 14:43:22 -0500

    Set up redis for get blogs route

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://y9m:Jjfs133$@ds127260.mlab.com:27260/blog_dev',
   cookieKey: '123123123'
 };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const Blog = mongoose.model('Blog');
 
@@ -14,8 +20,17 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const cachedBlogs = await client.get(req.user.id);
 
+    if (cachedBlogs) {
+      console.log('Serving from cache...');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    const blogs = await Blog.find({ _user: req.user.id });
+    client.set(req.user.id, JSON.stringify(blogs));
+
+    console.log('Serving from mongoDB...');
     res.send(blogs);
   });
 
