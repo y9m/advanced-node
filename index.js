c162a439ed1e9c271ecd126c4ffe661bd8c0e0b2
Jim Yang <jyang_123@hotmail.com>
2018-12-26 17:17:44 -0500

    Moved caching logic to cahce.js

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
+require('./services/cache');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(
+  keys.mongoURI,
+  { useMongoClient: true }
+);
 
 const app = express();
 
