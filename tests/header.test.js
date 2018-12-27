6a31f5c1aaf6f44e39018bebf0432e0a4bc9cebd
Jim Yang <jyang_123@hotmail.com>
2018-12-27 17:40:56 -0500

    Set up test for logging in using fake cookie

--- tests/header.test.js
+++ tests/header.test.js
@@ -18,3 +18,34 @@ test('The header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
 });
+
+test('Clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  const id = '5c23d60e70d61f36922fb836';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('sesison=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+  await page.waitFor('a[href="auth/logout]');
+
+  const text = await page.$eval('a[href="auth/logout"]', el => el.innerHTML);
+  expect(text).toEqual('Logout');
+});
