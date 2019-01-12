7702f94b93ab680267fd58e289a0dbdbf0a0c20d
Jim Yang <jyang_123@hotmail.com>
2019-01-12 12:49:52 -0500

    Created get route for s3 image uploading

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,10 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = {
+    file: null
+  };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -44,7 +48,7 @@ class BlogFormReview extends Component {
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
   }
 
   render() {
@@ -52,6 +56,13 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an Image</h5>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={event => this.setState({ file: event.target.files[0] })}
+        />
 
         {this.renderButtons()}
       </form>
@@ -63,4 +74,7 @@ function mapStateToProps(state) {
   return { formValues: state.form.blogForm.values };
 }
 
-export default connect(mapStateToProps, actions)(withRouter(BlogFormReview));
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(BlogFormReview));
--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://y9m:Jjfs133$@ds127260.mlab.com:27260/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAJX2UYC5NHJC3QGLQ',
+  secretAccessKey: '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'
 };
